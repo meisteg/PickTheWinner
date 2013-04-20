76a7aea1545d5636655b7af092d21b67f8612129
Greg Meiste <greg.meiste@gmail.com>
2013-04-20 08:54:07 -0500

    Update to Google Analytics 2.0beta5
    Signed-off-by: Greg Meiste <greg.meiste@gmail.com>

--- app/src/com/meiste/greg/ptw/PTW.java
+++ app/src/com/meiste/greg/ptw/PTW.java
@@ -15,12 +15,9 @@
  */
 package com.meiste.greg.ptw;
 
-import java.lang.Thread.UncaughtExceptionHandler;
-
 import android.app.Application;
 
 import com.google.analytics.tracking.android.EasyTracker;
-import com.google.analytics.tracking.android.ExceptionReporter;
 
 public class PTW extends Application {
 
@@ -41,13 +38,6 @@ public class PTW extends Application {
         Util.log("Application onCreate");
 
         EasyTracker.getInstance().setContext(this);
-
-        // Change uncaught exception parser...
-        final UncaughtExceptionHandler uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
-        if (uncaughtExceptionHandler instanceof ExceptionReporter) {
-            final ExceptionReporter exceptionReporter = (ExceptionReporter) uncaughtExceptionHandler;
-            exceptionReporter.setExceptionParser(new AnalyticsExceptionParser());
-        }
 
         /* HACK: Instantiate GAE class here so it can be used by activities and
          * services. For some reason, passing getApplicationContext() to GAE
