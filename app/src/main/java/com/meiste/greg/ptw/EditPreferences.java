0cf90d73b252515b6c6edd3dc2edebe83117487f
Greg Meiste <greg.meiste@gmail.com>
2014-07-26 20:22:54 -0500

    Switch from deprecated PreferenceActivity to PreferenceFragment
    Signed-off-by: Greg Meiste <greg.meiste@gmail.com>

--- app/src/main/java/com/meiste/greg/ptw/EditPreferences.java
+++ app/src/main/java/com/meiste/greg/ptw/EditPreferences.java
@@ -15,6 +15,7 @@
  */
 package com.meiste.greg.ptw;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -26,16 +27,14 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.text.TextUtils;
 import android.view.MenuItem;
 
-import com.google.android.gms.analytics.GoogleAnalytics;
-
-public class EditPreferences extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+public class EditPreferences extends BaseActivity {
     public static final String KEY_ACCOUNT_EMAIL = "account.email";
     public static final String KEY_ACCOUNT_COOKIE = "account.cookie";
 
@@ -52,109 +51,15 @@ public class EditPreferences extends PreferenceActivity implements OnSharedPrefe
 
     private static final int TAPS_TO_ENABLE_LOGGING = 7;
 
-    private int mLogHitCountdown;
-    private Preference mVibrate;
-
-    @SuppressWarnings("deprecation")
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mVibrate = findPreference(KEY_NOTIFY_VIBRATE);
-        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        if (vibrator == null || !vibrator.hasVibrator()) {
-            Util.log("Remove vibrator option since vibrator not present");
-            final PreferenceCategory pc = (PreferenceCategory)findPreference(KEY_REMINDER_SETTINGS);
-            pc.removePreference(mVibrate);
-            mVibrate = null;
-        }
-
-        findPreference(KEY_BUILD).setSummary(BuildConfig.VERSION_NAME);
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Util.log("EditPreferences.onResume");
-
-        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        prefs.registerOnSharedPreferenceChangeListener(this);
-        reminderCheck(prefs);
-        setRingtoneSummary(prefs);
-
-        final Preference account = findPreference(KEY_ACCOUNT_SCREEN);
-        account.setSummary(prefs.getString(KEY_ACCOUNT_EMAIL, getString(R.string.account_needed)));
-
-        mLogHitCountdown = TAPS_TO_ENABLE_LOGGING;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        GoogleAnalytics.getInstance(this).reportActivityStart(this);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        GoogleAnalytics.getInstance(this).reportActivityStop(this);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Util.log("EditPreferences.onPause");
-        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(final SharedPreferences prefs, final String key) {
-        reminderCheck(prefs);
-        if (key.equals(KEY_NOTIFY_RINGTONE)) {
-            setRingtoneSummary(prefs);
-        }
-    }
-
-    @SuppressWarnings("deprecation")
-    private void reminderCheck(final SharedPreferences prefs) {
-        if (prefs.getBoolean(KEY_NOTIFY_QUESTIONS, true) ||
-                prefs.getBoolean(KEY_NOTIFY_RACE, true) ||
-                prefs.getBoolean(KEY_NOTIFY_RESULTS, true)) {
-            findPreference(KEY_NOTIFY_LED).setEnabled(true);
-            findPreference(KEY_NOTIFY_RINGTONE).setEnabled(true);
-            if (mVibrate != null)
-                mVibrate.setEnabled(true);
-        } else {
-            findPreference(KEY_NOTIFY_LED).setEnabled(false);
-            findPreference(KEY_NOTIFY_RINGTONE).setEnabled(false);
-            if (mVibrate != null)
-                mVibrate.setEnabled(false);
-        }
-    }
-
-    @SuppressWarnings("deprecation")
-    private void setRingtoneSummary(final SharedPreferences prefs) {
-        String tone = prefs.getString(KEY_NOTIFY_RINGTONE, PTW.DEFAULT_NOTIFY_SND);
-        Preference preference = findPreference(KEY_NOTIFY_RINGTONE);
-
-        if (TextUtils.isEmpty(tone)) {
-            // Empty values correspond to 'silent' (no ringtone).
-            preference.setSummary(R.string.ringtone_silent);
-        } else {
-            Ringtone ringtone = RingtoneManager.getRingtone(
-                    preference.getContext(), Uri.parse(tone));
-
-            if (ringtone == null) {
-                // Set generic summary if there was a lookup error.
-                preference.setSummary(R.string.ringtone_summary);
-            } else {
-                // Set the summary to reflect the new ringtone display name.
-                preference.setSummary(ringtone.getTitle(preference.getContext()));
-            }
+        Fragment f = getFragmentManager().findFragmentById(android.R.id.content);
+        if (f == null || !(f instanceof SettingsFragment)) {
+            getFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new SettingsFragment()).commit();
         }
     }
 
@@ -169,19 +74,116 @@ public class EditPreferences extends PreferenceActivity implements OnSharedPrefe
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public boolean onPreferenceTreeClick(final PreferenceScreen preferenceScreen, final Preference preference) {
-        if (preference.getKey().equals(KEY_BUILD) && (mLogHitCountdown > 0)) {
-            mLogHitCountdown--;
-            if ((mLogHitCountdown == 0) && !Util.LOGGING_ENABLED) {
-                Util.LOGGING_ENABLED = true;
-                Util.log("Debug logging is temporarily enabled");
+    public static class SettingsFragment extends PreferenceFragment implements
+            OnSharedPreferenceChangeListener {
+
+        private int mLogHitCountdown;
+        private Preference mVibrate;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Util.log("SettingsFragment.onCreate");
+
+            addPreferencesFromResource(R.xml.preferences);
+
+            mVibrate = findPreference(KEY_NOTIFY_VIBRATE);
+            final Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+            if (vibrator == null || !vibrator.hasVibrator()) {
+                Util.log("Remove vibrator option since vibrator not present");
+                final PreferenceCategory pc = (PreferenceCategory)findPreference(KEY_REMINDER_SETTINGS);
+                pc.removePreference(mVibrate);
+                mVibrate = null;
             }
-        } else if (preference instanceof CheckBoxPreference) {
-            final String setting = ((CheckBoxPreference)preference).isChecked() ? "enable" : "disable";
-            Analytics.trackEvent(this, "Preferences", preference.getKey(), setting);
+
+            findPreference(KEY_BUILD).setSummary(BuildConfig.VERSION_NAME);
         }
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            Util.log("SettingsFragment.onResume");
+
+            final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            prefs.registerOnSharedPreferenceChangeListener(this);
+            reminderCheck(prefs);
+            setRingtoneSummary(prefs);
+
+            final Preference account = findPreference(KEY_ACCOUNT_SCREEN);
+            account.setSummary(prefs.getString(KEY_ACCOUNT_EMAIL, getString(R.string.account_needed)));
+
+            mLogHitCountdown = TAPS_TO_ENABLE_LOGGING;
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            Util.log("SettingsFragment.onPause");
+            PreferenceManager.getDefaultSharedPreferences(getActivity())
+                    .unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            Util.log("SettingsFragment.onDestroy");
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(final SharedPreferences prefs, final String key) {
+            if (key.equals(KEY_NOTIFY_RINGTONE)) {
+                setRingtoneSummary(prefs);
+            } else {
+                reminderCheck(prefs);
+            }
+        }
+
+        private void reminderCheck(final SharedPreferences prefs) {
+            boolean enabled = prefs.getBoolean(KEY_NOTIFY_QUESTIONS, true) ||
+                    prefs.getBoolean(KEY_NOTIFY_RACE, true) ||
+                    prefs.getBoolean(KEY_NOTIFY_RESULTS, true);
+            findPreference(KEY_NOTIFY_LED).setEnabled(enabled);
+            findPreference(KEY_NOTIFY_RINGTONE).setEnabled(enabled);
+            if (mVibrate != null) {
+                mVibrate.setEnabled(enabled);
+            }
+        }
+
+        private void setRingtoneSummary(final SharedPreferences prefs) {
+            String tone = prefs.getString(KEY_NOTIFY_RINGTONE, PTW.DEFAULT_NOTIFY_SND);
+            Preference preference = findPreference(KEY_NOTIFY_RINGTONE);
+
+            if (TextUtils.isEmpty(tone)) {
+                // Empty values correspond to 'silent' (no ringtone).
+                preference.setSummary(R.string.ringtone_silent);
+            } else {
+                Ringtone ringtone = RingtoneManager.getRingtone(
+                        preference.getContext(), Uri.parse(tone));
+
+                if (ringtone == null) {
+                    // Set generic summary if there was a lookup error.
+                    preference.setSummary(R.string.ringtone_summary);
+                } else {
+                    // Set the summary to reflect the new ringtone display name.
+                    preference.setSummary(ringtone.getTitle(preference.getContext()));
+                }
+            }
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(final PreferenceScreen preferenceScreen,
+                                             final Preference preference) {
+            if (preference.getKey().equals(KEY_BUILD) && (mLogHitCountdown > 0)) {
+                mLogHitCountdown--;
+                if ((mLogHitCountdown == 0) && !Util.LOGGING_ENABLED) {
+                    Util.LOGGING_ENABLED = true;
+                    Util.log("Debug logging is temporarily enabled");
+                }
+            } else if (preference instanceof CheckBoxPreference) {
+                final String setting = ((CheckBoxPreference)preference).isChecked() ? "enable" : "disable";
+                Analytics.trackEvent(getActivity(), "Preferences", preference.getKey(), setting);
+            }
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
     }
 }
