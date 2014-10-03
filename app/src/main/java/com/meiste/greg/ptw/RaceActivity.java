e1e8990df6b3f9965a6f8b429f88651cf0226536
Greg Meiste <greg.meiste@gmail.com>
2014-10-02 23:15:34 -0500

    Switch to Loader in RaceActivity. For #103
    Signed-off-by: Greg Meiste <greg.meiste@gmail.com>

--- app/src/main/java/com/meiste/greg/ptw/RaceActivity.java
+++ app/src/main/java/com/meiste/greg/ptw/RaceActivity.java
@@ -16,54 +16,103 @@
 package com.meiste.greg.ptw;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.MenuItem;
 
+import com.meiste.greg.ptw.provider.PtwContract;
 import com.meiste.greg.ptw.view.ControllableViewPager;
 
-public class RaceActivity extends BaseActivity {
+public class RaceActivity extends BaseActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     public static final String INTENT_ID = "race_id";
     public static final String INTENT_ALARM = "from_alarm";
 
     private ControllableViewPager mViewPager;
+    private RaceFragmentPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.race_details_main);
 
+        mAdapter = new RaceFragmentPagerAdapter();
         mViewPager = (ControllableViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(new RaceFragmentPagerAdapter());
-        mViewPager.setCurrentItem(getIntent().getExtras().getInt(INTENT_ID));
+        mViewPager.setAdapter(mAdapter);
 
         setDisplayHomeAsUpEnabled(true);
+        getSupportLoaderManager().initLoader(0, null, this);
     }
 
     @Override
     protected void onNewIntent(final Intent intent) {
         setIntent(intent);
-        mViewPager.setCurrentItem(getIntent().getExtras().getInt(INTENT_ID));
+        if (mAdapter.getCursor() != null) {
+            mViewPager.setCurrentItem(getIntent().getExtras().getInt(INTENT_ID), false);
+        }
     }
 
     public void onWorkaroundNeeded() {
         mViewPager.setPagingEnabled(false);
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(final int i, final Bundle bundle) {
+        return new CursorLoader(this,                                // Context
+                PtwContract.Race.CONTENT_URI,                        // URI
+                new String[]{PtwContract.Race.COLUMN_NAME_RACE_ID},  // Projection
+                null,                                                // Selection
+                null,                                                // Selection args
+                PtwContract.Race.DEFAULT_SORT);                      // Sort
+    }
+
+    @Override
+    public void onLoadFinished(final Loader<Cursor> cursorLoader, final Cursor cursor) {
+        final Cursor prevCursor = mAdapter.getCursor();
+        mAdapter.changeCursor(cursor);
+        if ((prevCursor == null) && (cursor != null)) {
+            mViewPager.setCurrentItem(getIntent().getExtras().getInt(INTENT_ID), false);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(final Loader<Cursor> cursorLoader) {
+        mAdapter.changeCursor(null);
+    }
+
     public class RaceFragmentPagerAdapter extends FragmentStatePagerAdapter {
+        private Cursor mCursor;
+
         public RaceFragmentPagerAdapter() {
             super(getSupportFragmentManager());
         }
 
         @Override
         public Fragment getItem(final int i) {
+            // Shouldn't happen
+            if (mCursor == null) return null;
+
             return RaceFragment.newInstance(i);
         }
 
         @Override
         public int getCount() {
-            return Races.get(getApplicationContext()).length;
+            return (mCursor == null) ? 0 : mCursor.getCount();
+        }
+
+        public void changeCursor(final Cursor c) {
+            if (mCursor != c) {
+                mCursor = c;
+                notifyDataSetChanged();
+            }
+        }
+
+        public Cursor getCursor() {
+            return mCursor;
         }
     }
 
